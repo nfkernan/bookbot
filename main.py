147c3718e38f84f91d995c29a36274867c9286b6
nfkernan <nf.kernan@gmail.com>
2024-06-29 05:05:08 -0400

    add letter count funct

--- main.py
+++ main.py
@@ -6,6 +6,20 @@ def main():
     words = file_contents.split()
     for word in words:
         number = number + 1
-    print(number)
+    print(f"This book contains {number} words.")
+
+def dict():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    alpha = file_contents.lower()
+    letters_count = {}
+    for l in alpha:
+        if l.isalpha():
+            if l in letters_count:
+                letters_count[l] += 1
+            else:
+                letters_count[l] = 1
+    print(letters_count)
 
 main()
+dict()
